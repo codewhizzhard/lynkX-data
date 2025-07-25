02b9eccc05519817365b5f9d27d611fd15ee93f6
codewhizzhard <topeadeolu5@gmail.com>
2025-07-25 23:58:33 +0100

    added more circle apis and logic to get address, balance and more

--- features/circle/circleController.js
+++ features/circle/circleController.js
@@ -19,52 +19,81 @@ const walletSetResponse = await client.createWalletSet({
 //console.log('Created WalletSet', walletSetResponse.data?.walletSet)
 const createWallet = async (req, res) => {
     try {
-        const { address, blockchains} = req.body;
+        const { address, blockchains, walletName} = req.body;
         if (!address) return res.status(400).json({message: "address is required"})
         const checksumAddress = getAddress(address)
-        if (!checksumAddress || !blockchains) {
+        if (!checksumAddress || !Array.isArray(blockchains) || blockchains.length === 0 || !name) {
             return res.status(400).json({message: "All fields are required to create a wallet"});
         }
         let user = await User.findOne({address: checksumAddress})
         if (!user) return res.status(400).json({message: "user not found"});
         //console.log("user:", user);
-        let count;
-        /* switch (user.role) {
-            case "merchant":
-                count = 2
-                block;
-            case "liquidity-provider":
-                count = 7
-                block;
-            case "treasury-manager":
-                count = 7
-                block;
-            default:
-                count = 2
-        } */
-       const countMap = {
-            merchant: 2,
-            liquidityProvider: 7,
-            treasuryManager: 7
-       }
+        if (user.wallets && user.wallets.length > 0) return res.status(200).json({message: "Wallet already created", user});
+       
         const walletsResponse = await client.createWallets({
         blockchains,
-        count: countMap[user.role] ?? 2,
+        count: 1,
         walletSetId: walletSetResponse.data?.walletSet?.id ?? '',
         })
-       
-        user.wallets = walletsResponse.data?.wallets || [];
+        const wallet = walletsResponse?.data?.wallets.map((wallet) => ({...wallet, walletName}))
+        user.wallets = wallet || [];
         await user.save();
-         console.log('Created Wallets', user)
+         //console.log('Created Wallets', walletsResponse?.data?.wallets.map((wallet) => ({...wallet, name: "just"})))
         
         return res.status(201).json({message: "wallet succesfully created", user})
         //console.log("user", {...user.toObject(), wallets: walletsResponse.data?.wallets})
         //if (walletSetResponse) return res.status(201).json({})
-
         //
+    } catch (err) {
+        console.log("err:", err)
+    }
+
+   
+}
+
+ const getAllUserWalletAddress = async(req, res) => {
+        try {
+            const {address} = req.params;
+            const checksumAddress = getAddress(address)
+            if (!checksumAddress) return res.status(400).json({message: "required a valid address"})
+            const user = await User.findOne({address: checksumAddress});
+            return res.status(200).json({message: "wallets fetched successfully", wallets: user?.wallets})
+            //console.log("user", user)
+        } catch (err) {
+            console.log("err:", err)
+        }
+}
+
+const getSpecificWallet = async (req, res) => {
+    try {
+        const {address, id} = req.params
+        const checksumAddress = getAddress(address);
+        if (!checksumAddress || !id) return res.status(401).json({message: "Required fields to filter out data"});
+        const user = await User.findOne({address: checksumAddress})
+        if (!user) return res.status(400).json({message: "User not found"});
+        const specificWallet = user.wallets.find((wallet) => id === wallet.id)
+        if (!specificWallet) return res.status(401).json({message: "Wallet not found"});
+        return res.status(200).json({message: "Wallet successfully fetched", specificWallet})
     } catch (err) {
         console.log("err:", err)
     }
 }
 
-export { createWallet }
+const getWalletBalance = async (req, res) => {
+    try {
+        const { id} = req.params;
+        if (!id) return res.status(400).json({message: "specify your request with the wallet id"});
+        const response = await client.getWalletTokenBalance({
+            id
+        })
+        if (!response) return res.status(401).json({message: "Wallet identity is not valid"});
+        res.status(200).json({message: "Wallet balance successfully fetched", walletBalance: response.data?.tokenBalances})
+        console.log("balance:", response.data?.tokenBalances)
+    } catch (err) {
+        console.log("err:", err);
+        if (err) return res.status(401).json({message: err});
+    }
+    
+}
+
+export { createWallet, getAllUserWalletAddress, getSpecificWallet, getWalletBalance}
--- features/circle/circleRoute.js
+++ features/circle/circleRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authmiddleware from "../user/userMiddleware.js";
-import { createWallet } from "./circleController.js";
+import { createWallet, getAllUserWalletAddress, getSpecificWallet, getWalletBalance } from "./circleController.js";
 
 const circleRouter = express.Router();
 
 circleRouter.use(authmiddleware)
 
 circleRouter.post("/create-wallets", createWallet);
+circleRouter.get("/getUserAddresses/:address", getAllUserWalletAddress);
+circleRouter.get("/getSpecificAddress/:address/:id", getSpecificWallet);
+circleRouter.get("/get-wallet-address/:id", getWalletBalance);
 
 export default circleRouter;
